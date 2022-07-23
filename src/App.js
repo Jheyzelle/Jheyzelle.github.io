7f6816888ce12308f9fcf2c8574231115d07475c
Jheyzelle <jzeldiaz@gmail.com>
2022-07-23 09:39:32 -0400

    Create landing section
    Animate landing section elements

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from './components/Contact';
 import TopNav from './components/TopNav';
 import About from './components/About';
 // import Projects from './components/Projects';
-// import Landing from './components/Landing';
+import Landing from './components/Landing';
 // import Footer from './components/Footer';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <span id='page-top'></span>
       <TopNav></TopNav>
-      {/* <Landing></Landing> */}
+      <Landing></Landing>
       <About></About>
       {/* <Projects></Projects> */}
       <Contact></Contact>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,74 @@
-import React from "react";
+import { wait } from "@testing-library/user-event/dist/utils";
+import React, { useEffect } from "react";
 
 function Landing() {
+    async function loadLanding() {
+        let landingLogo = document.querySelector('#landing-logo')
+        let landIntro = document.querySelector('#landing-intro')
+        let landRoles = document.querySelector('.roles')
+
+        typeText(['Hello there!', 'I am'], '#landing-intro', 100)
+
+        await wait(3700)
+        landingLogo.style.visibility = 'visible'
+        await wait(3300)
+        landingLogo.src = require('../assets/images/jheyzelle-logo.gif')
+
+
+        await wait(500)
+        // fade out welcome
+        landIntro.children[0].style.animation = 'fadeout-slide-left 2s 0.5s ease-in-out 1 forwards'
+        landIntro.children[1].style.animation = 'fadeout-slide-left 2s 0.7s ease-in-out 1 forwards'
+        // fade in roles text
+        landRoles.style.opacity = '1'
+        landRoles.children[0].style.animationPlayState = 'running'
+        landRoles.children[0].style.animationDelay = '0.2s'
+        landRoles.children[1].style.animationPlayState = 'running'
+        landRoles.children[1].style.animationDelay = '0.5s'
+        landRoles.children[2].style.animationPlayState = 'running'
+        landRoles.children[2].style.animationDelay = '0.9s'
+
+    }
+
+    async function typeText(lines, parentTag, timePerChar) {
+
+        let container = document.querySelector(parentTag)
+
+        for (let i = 0; i < lines.length; i++) {
+            // line
+            let line = lines[i]
+            let p = document.createElement('p')
+            container.appendChild(p)
+
+            for (let x = 0; x < line.length; x++) {
+                p.innerHTML += line[x]
+                await wait(timePerChar)
+            }
+            await wait(1000)
+        }
+    }
+
+    useEffect(() => {
+        loadLanding()
+    })
+
     return (
         <div className="nav-section section-landing">
+            <div className="landing-content">
+
+                <div id="landing-intro" className="fade-slide-left">
+                    {/* insert animated type text */}
+                </div>
+
+                <img alt="Jheyzelle logo" id='landing-logo' src={require('../assets/images/j-logo-write.gif')} draggable='false'></img>
+                <div className="roles">
+                    <p>Artist</p>
+                    <p>Designer</p>
+                    <p>Developer</p>
+                </div>
+
+            </div>
+
         </div>
     )
 }
